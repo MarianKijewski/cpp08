8d2a4a4e1cf4fbfbae63ff4a6771d8b8d951d141
MarianKijewski <mariankijewski@outlook.com>
2025-03-20 14:50:47 +0100

    Add better tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,17 +1,34 @@
 #include "Span.hpp"
 
+void	print_vec(const std::vector<int> & v)
+{
+	for (std::vector<int>::const_iterator i = v.begin(); i != v.end(); ++i)
+		std::cout << *i << ' ';
+	std::cout << std::endl;
+}
+
+int generate()
+{
+	const unsigned int	range = 100000;
+	const bool			positive = false;
+	if (positive)
+		return std::rand() % range;
+	else
+		return std::rand() % range - range / 2;
+}
+
 int main()
 {
-	Span sp = Span(7);
+	const unsigned int	N = 420000;
+	Span				sp = Span(N);
+	std::vector<int>	vec(N);
 
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-
-	std::vector<int> vec(2, 400);
+	srand(time(0));
+	int (*gen)() = generate;
+	std::generate(vec.begin(), vec.end(), gen);
+	// print_vec(vec);
 	sp.addNumber(vec.begin(), vec.end());
+
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
 
